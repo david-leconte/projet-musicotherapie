391b609fafc9ee94cacd8011a2512ecc9bcc80dc
David Leconte <David.Leconte2@etu.univ-paris1.fr>
2021-03-08 01:24:01 +0100

    Formulaire de contact fonctionnel

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'MAIL_PASSWORD' => NULL,
+  'MAIL_USER' => NULL,
+);
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+            
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('betview.conseil@gmail.com')
+                ->subject('Beya Musicothérapie : ' . $contactFormData['subject'])
+                ->text('Envoyé par : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['message'],
+                    'text/plain');
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé !');
+
+            return $this->redirectToRoute('contact');
+        }
+        
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
